1caa6e3fac559aed53674788fd1c617d57cb33cb
magali <digimaglink@gmail.com>
2021-03-21 16:19:42 +0100

    create routes

--- dwj-projet6/backend/app.js
+++ dwj-projet6/backend/app.js
@@ -1,12 +1,32 @@
 const express = require('express');
-
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const app = express();
+const path = require("path");
+const Sauce = require('./models/Sauce');
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://Magali-Valladier:Sopekocko@projet6.1xja1.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
 app.use(express.json());
 module.exports = app;
